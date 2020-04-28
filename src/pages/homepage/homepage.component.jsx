4a7c97d660c404f3e4fdae9c89bebb01ee0c4b3e
vitorsinedino <vitorsinedino@gmail.com>
2020-04-28 10:29:25 -0300

    destructuration of the components and creation of the styles

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../components/directory-component/directory";
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory></Directory>
+    </div>
+  );
+};
+
+export default HomePage;
