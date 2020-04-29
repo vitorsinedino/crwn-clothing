21002e7e7ae04504737cf0a17d9615ad56eeaa5a
vitorsinedino <vitorsinedino@gmail.com>
2020-04-29 13:45:01 -0300

    header and it's styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
+
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop-page/shop.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
